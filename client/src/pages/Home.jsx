41e4890d6a0f2e32c7b5f73a49b0b68ca3b641d7
Ludwig Pereira <ludwig.m.pereira@gmail.com>
2021-06-02 15:16:24 +0200

    added carrots

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,14 +2,32 @@ import React from "react";
 import axios from "axios";
 import ReactMapboxGl, { Marker } from "react-mapbox-gl";
 import apiHandler from "../api/apiHandler";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 
   const Map = ReactMapboxGl({
     accessToken: process.env.REACT_APP_MAPBOX_TOKEN,
   });
   
-  const Home = (props) => {
+  class Home extends React.Component {
+state={
+  items:[],
+};
 
+componentDidMount() {
+axios
+.get(process.env.REACT_APP_BACKEND_URL + "/api/items")
+.then((response)=>{
+  this.setState({
+items:response.data
+});
+})
+.catch((error)=>{
+  console.log(error);
+});
+}
+render() {
+  console.log(this.state.items)
   return (
     <div>
       <h1> MAPBOX HERE</h1>
@@ -22,10 +40,19 @@ import apiHandler from "../api/apiHandler";
     width: '100vw'
   }}
 >
-
+<Marker
+  coordinates={[2.3522,48.8566]}
+  anchor="bottom">
+  <img style={{
+    width:24,
+    height:24,
+  }}
+  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Carrot-fb.jpg/800px-Carrot-fb.jpg"
+  alt="carrots"/>
+</Marker>
 </Map>;   
 </div>
   );
 };
-
+}
 export default Home;
