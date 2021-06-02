95ad8c7a8a5120da83ccf0309751e447258cd6fe
Ludwig Pereira <ludwig.m.pereira@gmail.com>
2021-06-02 13:04:55 +0200

    started home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,30 @@
 import React from "react";
+import axios from "axios";
+import ReactMapboxGl, { Marker } from "react-mapbox-gl";
+import apiHandler from "../api/apiHandler";
 
-const Home = (props) => {
-  // Implement react map box here.
+
+  const Map = ReactMapboxGl({
+    accessToken: process.env.REACT_APP_MAPBOX_TOKEN,
+  });
+  
+  const Home = (props) => {
+
   return (
     <div>
-      <h1>MAPBOX MAP HERE</h1>
-      <p>On home /</p>
-    </div>
+      <h1> MAPBOX HERE</h1>
+      <Map
+  center={[2.3522,48.8566]}
+  zoom={[14]}
+  style="mapbox://styles/mapbox/streets-v9"
+  containerStyle={{
+    height: '100vh',
+    width: '100vw'
+  }}
+>
+
+</Map>;   
+</div>
   );
 };
 
